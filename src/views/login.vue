25f22fad51266a10f4b3ed77774f44f22744a36e
dinglun <dinglun@yy.com>
2019-12-10 12:45:29 +0800

    技术的v

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class='wrap'>
-    <p class="title">伦哥网站登录页面,git push</p>
+    <p class="title">伦哥网站登录页面,git push页面</p>
     <div style="margin-top: 160px;">
       <van-field
         v-model="name"
